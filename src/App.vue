ca8d32c0d8a126131309ff44b80bedca69509323
김아진 <aj.kim@stickint.kr>
2021-04-01 16:53:11 +0900

    route-link steylGuide
    직접 주소로 이동하면 404, (해당 페이지 블로그 본적 있음 다시 링크 찾아보기)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
-<div id="nav">
+    <!-- [TODO] router-link로 이동하면 뜨는데, 직접 페이지 치면 안나오는 이슈: 블로그 본 거 같은데 다시 카톡 뒤지기 -->
+    <div id="sample-nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      <router-link to="/styleGuide">StyleGuide</router-link>
+      <router-link to="/styleGuide" style="margin: 10px; font-size; 16px; font-family: Monsterate; color: #ccc;">StyleGuide</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+#sample-nav {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+}
 // #app {
 //   font-family: Avenir, Helvetica, Arial, sans-serif;
 //   -webkit-font-smoothing: antialiased;
