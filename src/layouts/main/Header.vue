5c83301d815663e5bfa5ea290c6f6f65cc809e64
김아진 <aj.kim@stickint.kr>
2021-04-08 18:30:29 +0900

    empty layout

---
+++ src/layouts/main/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header id="globalHeader" role="banner" style="position: fixed;">
+    <!-- <div id="siteLogo">
+        <a href="/">
+            <img class="replace" src="@/assets/logo.png" alt="POLA">
+        </a>
+    </div>
+
+    <div id="btnMenu">
+        <a class="menu-trigger circle" href="javascript:void(0)" onmousedown="ga('send', 'event', 'ctop','glonavi','ctop_glonavi');" onclick="ga('send', 'event', 'ctop','glonavi','ctop_glonavi');">
+      <div class="menu_mask animation"></div>
+      <div class="ic animation">
+        <span class="ic-child01"></span>
+        <span class="ic-child02"></span>
+        <span class="ic-child03"></span>
+      </div>
+      <div class="close">
+        <span class="close-child01"></span>
+        <span class="close-child01"></span>
+      </div>
+    </a></div> -->
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/layouts/main/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="main-layout">
+        this is pages/index/app.vue
+    <main-header></main-header>
+  </div>
+</template>
+
+<script>
+import MainHeader from "@/layouts/main/Header"
+import MainContent from "@/layouts/main/Content"
+import MainFooter from "@/layouts/main/Footer"
+
+export default {
+    name: 'MainLayout',
+    components: { MainHeader, MainContent, MainFooter },
+}
+</script>
+
+<style lang="scss">
+</style>
